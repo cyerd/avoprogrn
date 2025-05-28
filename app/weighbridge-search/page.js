86c56071573b84232c6596901f1b0046e5184c91
hudheyfa <hudheyfa@gmail.com>
2025-05-28 15:17:30 +0300

    add build updates

--- app/weighbridge-search/page.js
+++ app/weighbridge-search/page.js
@@ -1,15 +1,18 @@
 import Pagination from "@/components/Pagination";
 import ResultsTable from "@/components/ResultsTable";
 import SearchForm from "@/components/SearchForm";
+import { Suspense } from "react";
 
 
 export default function WeighbridgeSearchPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Weighbridge Search</h1>
+      <Suspense fallback={<div>Loading...</div>}>
       <SearchForm />
       <ResultsTable />
       <Pagination />
+      </Suspense>
     </div>
   );
 }
