411575f9f75249823fad597fac3c88993c889274
Hudheyfa Cyerd <hudheyfa@gmail.com>
2025-05-28 15:05:55 +0300

    suspense

--- app/grn-form/page.js
+++ app/grn-form/page.js
@@ -1,10 +1,14 @@
 import GRNForm from '@/components/GRNForm'
+import { Suspense } from 'react';
+
 
 export default function GRNFormPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Goods Received Note</h1>
+    <Suspense>
       <GRNForm />
+    </Suspense>
     </div>
   );
 }
